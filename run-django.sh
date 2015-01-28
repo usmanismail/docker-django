ace2c177d0261236e4bd6c8475ada50c76cbcbbd
Usman Ismail <usman.ismail@maluuba.com>
2015-01-27 20:38:34 -0500

    Update run-django.sh

--- run-django.sh
+++ run-django.sh
@@ -7,10 +7,7 @@ cp /tmp/uwsgi_params /var/site/
 # Run nginx in the background
 nginx 
 
-# Run wsgi in the foreground
-cd /var/site/${SITE_NAME}
-
 python manage.py collectstatic
 cp -R static ../
 
-uwsgi --wsgi-file ${SITE_NAME}/wsgi.py -s /var/run/wsgi.sock --chmod-socket=666
+uwsgi --wsgi-file wsgi.py -s /var/run/wsgi.sock --chmod-socket=666
